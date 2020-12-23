ef38d696ee6cc2364acc47538bce6c8a6a232785
shibin <shibin.you@qq.com>
2020-12-23 23:37:49 +0800

    feat: 🎸 init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+console.log()
+module.exports = {
+    title: 'Web notes',
+    description: 'Web 前端学习笔记',
+    base: process.env.NODE_ENV === 'production' ? '/web-notes/' : '/',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Github', link: 'https://github.com/shibin-cli/web-notes' },
+        ],
+        sidebar: [
+           [ '/analysis','需求和架构设计'],
+        ]
+    },
+    plugins: ['@vuepress/pwa', {
+        serviceWorker: true,
+        updatePopup: true
+    }],
+}
